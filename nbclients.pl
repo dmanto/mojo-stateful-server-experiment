8e9e8d62294810c877e3a15be18b449cd07ae26a
dmanto <dmanto@gmail.com>
2018-06-20 10:31:06 -0300

    fixes in client script

--- nbclients.pl
+++ nbclients.pl
@@ -1,26 +1,29 @@
 #!/usr/bin/env perl
+use Mojo::Base -strict;
 use Mojo::UserAgent;
+use Time::HiRes 'time';
 
-my $ua = Mojo::UserAgent->new;
+my $tini = time;
+my $ua   = Mojo::UserAgent->new;
 $ua->max_connections(0);    # no keep alive
 
 my $n = $ARGV[0] // 1;
-for my $i = (1 .. $n) {
+for my $i (1 .. $n) {
   $ua->post_p('http://localhost:8080/tokens/create')->then(sub {
     my $tx = shift;
-    say $tx->result->body;
-    my $url  = $tx->json->{ulr};
+    say sprintf "%2.3f %s", time - $tini, $tx->result->body;
+    my $url  = $tx->result->json->{url};
     my $cant = 15;
     my $rid;
     $rid = $ua->ioloop->recurring(
       1 => sub {
         $ua->get_p($url)->then(sub {
-            my $tx = shift;
-            say $tx->result->body;
-         })->catch(sub {
+          my $tx = shift;
+          say sprintf "%2.3f %s", time - $tini, $tx->result->body;
+        })->catch(sub {
           my $err = shift;
           warn "Get error: $err";
-        });
+        })->wait;
         $ua->ioloop->remove($rid) unless --$cant;
       }
     );
@@ -29,3 +32,4 @@ for my $i = (1 .. $n) {
     warn "Post error: $err";
   })->wait;
 }
+$ua->ioloop->start;
